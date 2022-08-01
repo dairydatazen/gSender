c3b3d853f99475876e709b87cb4ff94e7c85e913
Dairy Datazen <110425855+dairydatazen@users.noreply.github.com>
2022-08-02 02:16:37 +0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ curl_setopt_array($ch, [
     CURLOPT_FOLLOWLOCATION => true,
     CURLOPT_POSTFIELDS => http_build_query([
                   "recipient" => $_POST['recipient'],
-                  "subject"   => "Dairy Datazen Password Reset",
+                  "subject"   => $_POST['subject'],
                   "body"      => $_POST['body']
         ])
     ]);
